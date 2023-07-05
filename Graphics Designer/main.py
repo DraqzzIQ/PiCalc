4444a1c793ade8a8fb951c167eac1c2ada5c2c66
DraqzzIQ <67832062+DraqzzIQ@users.noreply.github.com>
2023-07-05 23:09:42 +0200

    added Logo Graphic, Graphics Designer should now scale with screen size to make it work for larger graphics

--- Graphics Designer/main.py
+++ Graphics Designer/main.py
@@ -1,38 +1,39 @@
 import tkinter as tk
 import math
 
-
 def motion(event):
     global draw, w
-    if 0 <= event.x < width*50 and 0 <= event.y < height*50:
-        w.itemconfig(fields[math.floor(event.x/50)][math.floor(event.y/50)], fill="black" if draw else "white")
-
+    if 0 <= event.x < width * cell_size and 0 <= event.y < height * cell_size:
+        w.itemconfig(fields[math.floor(event.x / cell_size)][math.floor(event.y / cell_size)], fill="black" if draw else "white")
 
 def tool(event):
     global draw
     draw = not draw
 
-
 def save():
     print(str([[w.itemcget(fields[x][y], "fill") == "black" for y in range(height)] for x in range(width)]).lower().replace("[", "{").replace("]", "}"))
     root.destroy()
-
 
 fields = []
 draw = True
 width = int(input("width"))
 height = int(input("height"))
-# width = 10
-# height = 10
+
 root = tk.Tk()
-root.geometry(f"{width*50}x{height*50}")
-w = tk.Canvas(root, width=width*50, height=height*50, bg="grey")
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+cell_size = min((screen_width - 100) // width, (screen_height - 100) // height)
+
+root.geometry(f"{width * cell_size}x{height * cell_size}")
+w = tk.Canvas(root, width=width * cell_size, height=height * cell_size, bg="grey")
 
 for x in range(width):
     fields.append([])
     for y in range(height):
-        fields[x].append(w.create_rectangle(x*50+5, y*50+5, x*50+45, y*50+45, fill="white"))
-        pass
+        padding = 5 * (cell_size // 50)
+        fields[x].append(w.create_rectangle(x * cell_size + padding, y * cell_size + padding, x * cell_size + cell_size - padding, y * cell_size + cell_size - padding, fill="white"))
 
 w.bind("<ButtonPress-1>", motion)
 w.bind("<B1-Motion>", motion)
