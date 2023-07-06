863da421be040e7ea095d3ce32ff7d6765a628de
Raphael <raphael.kastenmeier@gmail.com>
2023-07-06 18:37:56 +0200

    Added Size Changing for Graphics designer

--- Graphics Designer/main.py
+++ Graphics Designer/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import math
 
 def motion(event):
-    if 0 <= event.x < len(fields) * cell_size and 0 <= event.y < len(fields[0]) * cell_size:
+    if 0 <= event.x < width * cell_size and 0 <= event.y < height * cell_size:
         w.itemconfig(fields[math.floor(event.x / cell_size)][math.floor(event.y / cell_size)], fill="black" if draw else "white")
 
 def tool(event):
@@ -19,7 +19,7 @@ def close():
     root.destroy()
 
 def updateSize(event):
-    global cell_size, fields
+    global cell_size, fields, padding
     cell_size = min(math.floor(root.winfo_width()/width), math.floor(root.winfo_height()/height))
     padding = cell_size // 10
 
@@ -28,6 +28,40 @@ def updateSize(event):
     for x in range(width):
         for y in range(height):
             w.coords(fields[x][y], x * cell_size + padding, y * cell_size + padding, x * cell_size + cell_size - padding, y * cell_size + cell_size - padding)
+
+def addColumn(event):
+    global width
+    width += 1
+    fields.append([w.create_rectangle(0, 0, 0, 0, fill="white") for _ in range(height)])
+    updateSize(0)
+
+def removeColumn(event):
+    global width
+    if width > 1:
+        width -= 1
+        for y in range(height-1, -1, -1):
+            print(y)
+            w.delete(fields[-1][y])
+        del fields[-1]
+        updateSize(0)
+        print(fields, height)
+
+def addRow(event):
+    global height
+    height += 1
+    for x in range(width):
+        fields[x].append(w.create_rectangle(0, 0, 0, 0, fill="white"))
+    updateSize(0)
+
+def removeRow(event):
+    global height
+    if height > 1:
+        height -= 1
+        for x in range(width):
+            w.delete(fields[x][-1])
+            del fields[x][-1]
+        updateSize(0)
+        print(fields, height)
 
 draw = True
 width = int(input("width: "))
@@ -52,6 +86,10 @@ w.bind("<B1-Motion>", motion)
 root.bind("<space>", tool)
 root.bind("<Escape>", save)
 root.bind("<Configure>", updateSize)
+root.bind("<Right>", addColumn)
+root.bind("<Left>", removeColumn)
+root.bind("<Down>", addRow)
+root.bind("<Up>", removeRow)
 root.protocol("WM_DELETE_WINDOW", close)
 w.pack()
 root.mainloop()
