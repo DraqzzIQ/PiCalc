1f5215e2ebaf49183d92eb74d564757b2df5eab2
Raphael <raphael.kastenmeier@gmail.com>
2023-07-08 13:08:17 +0200

    made spacing parameter of create_text do something

--- Graphics.cpp
+++ Graphics.cpp
@@ -9,21 +9,25 @@ Graphics::~Graphics()
 {
 }
 
-render_plane Graphics::create_text(const std::string text, int spacing, std::map<wchar_t, render_plane> table) {
+render_plane Graphics::create_text(const std::string text, std::map<wchar_t, render_plane> table, int spacing) {
 	render_plane Text = table.at(text.at(0));
 	std::vector<bool> empty = std::vector<bool>(Text.at(0).size(), false);
-	for (int i = 1; i < text.length(); i++) {
-		Text.push_back(empty);
+	for (size_t i = 1; i < text.length(); i++) {
+		for (size_t i = 0; i < spacing; i++) {
+			Text.push_back(empty);
+		}
 		Text.insert(Text.end(), table.at(text.at(i)).begin(), table.at(text.at(i)).end());
 	}
 	return Text;
 }
 
-render_plane Graphics::create_text(const std::wstring text, int spacing, std::map<wchar_t, render_plane> table) {
+render_plane Graphics::create_text(const std::wstring text, std::map<wchar_t, render_plane> table, int spacing) {
 	render_plane Text = table.at(text.at(0));
 	std::vector<bool> empty = std::vector<bool>(Text.at(0).size(), false);
-	for (int i = 1; i < text.length(); i++) {
-		Text.push_back(empty);
+	for (size_t i = 1; i < text.length(); i++) {
+		for (size_t i = 0; i < spacing; i++) {
+			Text.push_back(empty);
+		}
 		Text.insert(Text.end(), table.at(text.at(i)).begin(), table.at(text.at(i)).end());
 	}
 	return Text;
