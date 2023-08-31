36746a587e5333ac2031474b07964c27f78478d8
DraqzzIQ <kgerhards03@gmail.com>
2023-08-31 14:06:32 +0200

    compilation fix for gcc

--- Equation.h
+++ Equation.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "Window.h"
 #include "Graphics.h"
+#include "KeyPress.h"
 #include <vector>
 #include <string>
 #include <map>
 #include <cmath>
-#include "KeyPress.h"
+#include <algorithm>
 
 
 class Equation
