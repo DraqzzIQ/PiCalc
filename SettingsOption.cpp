00faad1efd97c9752f657df9195f35fdc3ed30c9
DraqzzIQ <kgerhards03@gmail.com>
2023-09-09 13:59:03 +0200

    fixed wrong window selection

--- SettingsOption.cpp
+++ SettingsOption.cpp
@@ -2,13 +2,13 @@
 
 void SettingsOption::on_select()
 {
-    window_manager->add_window(new SettingsWindow(window_manager));
-}
-
-void SettingsOption::on_shift_select()
-{
     if(window_manager->has_window<SettingsWindow>())
         window_manager->open_window<SettingsWindow>();
     else
         window_manager->add_window(new SettingsWindow(window_manager));
+}
+
+void SettingsOption::on_shift_select()
+{
+    window_manager->add_window(new SettingsWindow(window_manager));
 }
--- SysInfoOption.cpp
+++ SysInfoOption.cpp
@@ -2,13 +2,13 @@
 
 void SysInfoOption::on_select()
 {
-    window_manager->add_window(new SysInfoWindow());
-}
-
-void SysInfoOption::on_shift_select()
-{
     if(window_manager->has_window<SysInfoWindow>())
         window_manager->open_window<SysInfoWindow>();
     else
         window_manager->add_window(new SysInfoWindow());
+}
+
+void SysInfoOption::on_shift_select()
+{
+    window_manager->add_window(new SysInfoWindow());
 }
