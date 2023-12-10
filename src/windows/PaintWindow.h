8a0386849dfe40ee1f1b14b0eecdb8a51197863d
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-12-10 22:05:26 +0100

    feat: not working paint option

---
+++ src/windows/PaintWindow.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "constant/Graphics.h"
+#include "windows/Window.h"
+#include <iostream>
+
+class PaintWindow: public Window
+{
+	public:
+	PaintWindow();
+	~PaintWindow();
+	Bitset2D update_window();
+	void handle_key_down(KeyPress keypress);
+
+	private:
+	Bitset2D _rendered;
+	int _cursor[2] = {0, 0};
+	bool _pen_down = false;
+};
