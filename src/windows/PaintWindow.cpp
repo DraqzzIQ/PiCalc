1a06f3a23922298aa98137215eb8e5613b0c79b2
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-12-10 22:40:22 +0100

    fix: can now use SCREEN_WIDTH correcly

--- src/windows/PaintWindow.cpp
+++ src/windows/PaintWindow.cpp
@@ -2,15 +2,17 @@
 
 PaintWindow::PaintWindow()
 {
-    _rendered = Bitset2D(Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT, false);
-	_cursor[0] = Constants::SCREEN_WIDTH / 2;
-	_cursor[1] = Constants::SCREEN_HEIGHT / 2;
+    painted = Bitset2D(SCREEN_WIDTH, SCREEN_HEIGHT, false);
+	_cursor[0] = SCREEN_WIDTH / 2;
+	_cursor[1] = SCREEN_HEIGHT / 2;
 }
 
 PaintWindow::~PaintWindow() {}
 
 Bitset2D PaintWindow::update_window()
 {
+	Bitset2D _rendered = painted;
+	_rendered.set_bit(_cursor[0], _cursor[1], true);
 	return _rendered;
 }
 
@@ -22,7 +24,7 @@ void PaintWindow::handle_key_down(KeyPress keypress)
     }
     else if (keypress.key_calculator == Chars::KEY_MAP.at('up')) {
         _cursor[1]++;
-        if (_cursor[1] >= Constants::SCREEN_HEIGHT) _cursor[1] = Constants::SCREEN_HEIGHT - 1;
+        if (_cursor[1] >= SCREEN_HEIGHT) _cursor[1] = SCREEN_HEIGHT - 1;
     }
     else if (keypress.key_calculator == Chars::KEY_MAP.at('left')) {
         _cursor[0]--;
@@ -30,13 +32,13 @@ void PaintWindow::handle_key_down(KeyPress keypress)
     }
     else if (keypress.key_calculator == Chars::KEY_MAP.at('right')) {
         _cursor[0]++;
-        if (_cursor[0] >= Constants::SCREEN_WIDTH) _cursor[0] = Constants::SCREEN_WIDTH - 1;
+        if (_cursor[0] >= SCREEN_WIDTH) _cursor[0] = SCREEN_WIDTH - 1;
     }
     else if (keypress.key_calculator == Chars::KEY_MAP.at('=')) {
         _pen_down = !_pen_down;
     }
     else if (keypress.key_calculator == Chars::KEY_MAP.at('AC')) {
-        _rendered = Bitset2D(Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT, false);
+        painted = Bitset2D(SCREEN_WIDTH, SCREEN_HEIGHT, false);
     }
-	// if (_pen_down) _rendered.set(_cursor[0], _cursor[1], true);
+	if (_pen_down) painted.set_bit(_cursor[0], _cursor[1], true);
 }
