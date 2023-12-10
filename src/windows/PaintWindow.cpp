25eefd3e3b27d961110adc09c428cbc50c700e31
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2023-12-10 22:47:18 +0100

    feat: working paint. (no save possible)

--- src/windows/PaintWindow.cpp
+++ src/windows/PaintWindow.cpp
@@ -18,26 +18,26 @@ Bitset2D PaintWindow::update_window()
 
 void PaintWindow::handle_key_down(KeyPress keypress)
 {
-	if (keypress.key_calculator == Chars::KEY_MAP.at('down')) {
+	if (keypress.key_calculator == Chars::KEY_MAP.at("up")) {
         _cursor[1]--;
         if (_cursor[1] < 0) _cursor[1] = 0;
     }
-    else if (keypress.key_calculator == Chars::KEY_MAP.at('up')) {
+    else if (keypress.key_calculator == Chars::KEY_MAP.at("down")) {
         _cursor[1]++;
         if (_cursor[1] >= SCREEN_HEIGHT) _cursor[1] = SCREEN_HEIGHT - 1;
     }
-    else if (keypress.key_calculator == Chars::KEY_MAP.at('left')) {
+    else if (keypress.key_calculator == Chars::KEY_MAP.at("left")) {
         _cursor[0]--;
         if (_cursor[0] < 0) _cursor[0] = 0;
     }
-    else if (keypress.key_calculator == Chars::KEY_MAP.at('right')) {
+    else if (keypress.key_calculator == Chars::KEY_MAP.at("right")) {
         _cursor[0]++;
         if (_cursor[0] >= SCREEN_WIDTH) _cursor[0] = SCREEN_WIDTH - 1;
     }
-    else if (keypress.key_calculator == Chars::KEY_MAP.at('=')) {
+    else if (keypress.key_calculator == Chars::KEY_MAP.at("=")) {
         _pen_down = !_pen_down;
     }
-    else if (keypress.key_calculator == Chars::KEY_MAP.at('AC')) {
+    else if (keypress.key_calculator == Chars::KEY_MAP.at("AC")) {
         painted = Bitset2D(SCREEN_WIDTH, SCREEN_HEIGHT, false);
     }
 	if (_pen_down) painted.set_bit(_cursor[0], _cursor[1], true);
