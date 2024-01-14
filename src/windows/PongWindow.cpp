bb07ed2043918b28848ed14d73224ebe7b94a70d
Raphael <raphael.kastenmeier@gmail.com>
2024-01-14 21:10:13 +0100

    started adding Pong, now focussing on finishing the Calculator

---
+++ src/windows/PongWindow.cpp
@@ -0,0 +1,20 @@
+#include "PongWindow.h"
+
+PongWindow::PongWindow()
+{
+	_menu = Menu::GAME;
+	_settings = 0b1000;
+}
+
+PongWindow::~PongWindow()
+{
+}
+
+Bitset2D PongWindow::update_window()
+{
+	return Bitset2D();
+}
+
+bool PongWindow::handle_key_down(KeyPress keypress)
+{
+}
