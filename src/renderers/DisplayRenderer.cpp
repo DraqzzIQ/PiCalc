919f825a0c994043a27c3549efbdf97a3b1b6ac9
Raphael <raphael.kastenmeier@gmail.com>
2024-01-18 16:16:28 +0100

    pico display fix

--- src/renderers/DisplayRenderer.cpp
+++ src/renderers/DisplayRenderer.cpp
@@ -25,14 +25,16 @@ void DisplayRenderer::render(const Bitset2D& pixels, const DynamicBitset& screen
 	for (uint8_t j = 0; j < pixels.width(); j++) {
 		std::vector<uint8_t> bytes = pixels[j].get_bytes();
 
+		// comment this
 		bytes[3] >>= 1;
 		if (bytes[2] & 1) bytes[3] |= 0x80;
 		bytes[2] >>= 1;
 		if (bytes[1] & 1) bytes[2] |= 0x80;
 		bytes[1] >>= 1;
 		if (bytes[0] & 1) bytes[1] |= 0x80;
-		if (j == screen_symbol_index && screen_symbols.at(screen_symbol_index++) == 1) bytes[0] |= 1;
+		if (j == screen_symbol_positions[screen_symbol_index] && screen_symbols.at(screen_symbol_positions[screen_symbol_index++]) == 1) bytes[0] |= 1;
 		else bytes[0] &= 0xFE;
+		// to here
 
 		for (uint8_t i = 0; i < 4; i++) {
 			command[index++] = reverse_byte(bytes[i]);
