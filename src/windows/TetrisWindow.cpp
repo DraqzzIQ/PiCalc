a94b67102fe28aab7d263807c9c08c88b0bfb686
Karl Gerhards <67832062+DraqzzIQ@users.noreply.github.com>
2024-01-30 17:11:48 +0100

    Tetris done

--- src/windows/TetrisWindow.cpp
+++ src/windows/TetrisWindow.cpp
@@ -2,9 +2,10 @@
 
 TetrisWindow::TetrisWindow()
 {
-	board = Bitset2D(20, 32, false);
+	_board = Bitset2D(_board_width, 31, false);
 	_last_update = Utils::us_since_boot();
 	srand(Utils::us_since_boot());
+	_next_piece = _pieces[rand() % 7];
 	spawn_piece();
 }
 
@@ -16,9 +17,30 @@ Frame TetrisWindow::update_window()
 {
 	clear_window();
 
-	add_to_window(Bitset2D(1, 32, true), 37, 0);
-	add_to_window(board, 38, 0);
-	add_to_window(Bitset2D(1, 32, true), 59, 0);
+	add_to_window(Graphics::create_text("Hold:", Graphics::SYMBOLS_6_HIGH), 3, 0);
+	add_to_window(_hold_piece, 14, 7);
+
+	add_to_window(Graphics::create_text("Score:", Graphics::SYMBOLS_6_HIGH), 2, 15);
+	add_to_window(Graphics::create_text(std::to_string(_score), Graphics::SYMBOLS_6_HIGH), 3, 23);
+
+	add_to_window(Bitset2D(2, 31, true), _board_offset - 2, 0);
+	add_to_window(_board, _board_offset, 0);
+	add_to_window(Bitset2D(2, 31, true), _board_offset + _board_width, 0);
+
+	add_to_window(Graphics::create_text("Next:", Graphics::SYMBOLS_6_HIGH), _board_offset + _board_width + 5, 0);
+	add_to_window(_next_piece, _board_offset + _board_width + 16, 7);
+
+	add_to_window(Graphics::create_text("Lines:", Graphics::SYMBOLS_6_HIGH), _board_offset + _board_width + 3, 15);
+	add_to_window(Graphics::create_text(std::to_string(_lines), Graphics::SYMBOLS_6_HIGH), _board_offset + _board_width + 4, 23);
+
+
+	for (uint8_t i = 0; i < _falling_piece.width(); i++) {
+		for (uint8_t j = 0; j < _falling_piece.height(); j++) {
+			if (_falling_piece.get_bit(i, j)) {
+				_window.set_bit(_falling_piece_x + i, _falling_piece_y + j, true);
+			}
+		}
+	}
 
 	if (Utils::us_since_boot() - _last_update > 1000000) {
 		move_piece_down();
@@ -30,28 +52,168 @@ Frame TetrisWindow::update_window()
 
 bool TetrisWindow::handle_key_down(KeyPress keypress)
 {
+	if (keypress.key_raw == Chars::CHAR_TO_KEYCODE.at("left")) {
+		move_piece_left();
+	} else if (keypress.key_raw == Chars::CHAR_TO_KEYCODE.at("right")) {
+		move_piece_right();
+	} else if (keypress.key_raw == Chars::CHAR_TO_KEYCODE.at("down")) {
+		_score += 1;
+		move_piece_down();
+	} else if (keypress.key_raw == Chars::CHAR_TO_KEYCODE.at("=")) {
+		hold();
+	} else if (keypress.key_raw == Chars::CHAR_TO_KEYCODE.at("SHIFT")) {
+		rotate_piece();
+	}
+
 	return true;
 }
 
 void TetrisWindow::spawn_piece()
 {
+	_falling_piece = _next_piece;
+	uint8_t offset = 48 - _falling_piece.width() / 2;
+	if (offset % 2 == 1) offset--;
+	_falling_piece_x = offset;
+	_falling_piece_y = 1;
+	if (touching_piece()) {
+		// game over
+		_board = Bitset2D(_board_width, 31, false);
+		_score = 0;
+		_lines = 0;
+	}
+
 	int index = rand() % 7;
-	Bitset2D piece = peices[index];
-	board.insert_x(0, piece);
+	_next_piece = _pieces[index];
 }
 
 void TetrisWindow::move_piece_down()
 {
+	if (_falling_piece_y + _falling_piece.height() < _board.height() && !touching_piece(0, 1)) {
+		_falling_piece_y += 2;
+		return;
+	}
+
+	uint8_t local_x = _falling_piece_x - _board_offset;
+
+	for (uint8_t i = 0; i < _falling_piece.width(); i++) {
+		for (uint8_t j = 0; j < _falling_piece.height(); j++) {
+			if (_falling_piece.get_bit(i, j)) {
+				_board.set_bit(local_x + i, _falling_piece_y + j, true);
+			}
+		}
+	}
+
+	check_for_lines();
+	spawn_piece();
+}
+
+void TetrisWindow::check_for_lines()
+{
+	uint8_t lines = 0;
+
+	for (uint8_t i = 0; i < _board.height(); i += 2) {
+		bool line = true;
+		for (uint8_t j = 0; j < _board.width(); j++) {
+			if (!_board.get_bit(j, i)) {
+				line = false;
+				break;
+			}
+		}
+
+
+		if (line) {
+			lines++;
+			for (uint8_t j = i; j > 1; j -= 1) {
+				for (uint8_t k = 0; k < _board.width(); k++) {
+					_board.set_bit(k, j, _board.get_bit(k, j - 2));
+				}
+			}
+		}
+	}
+
+	_lines += lines;
+	switch (lines) {
+	case 1:
+		_score += 100;
+		break;
+	case 2:
+		_score += 300;
+		break;
+	case 3:
+		_score += 500;
+		break;
+	case 4:
+		_score += 800;
+		break;
+	default:
+		break;
+	}
+}
+
+bool TetrisWindow::touching_piece(int8_t offset_x, int8_t offset_y)
+{
+	uint8_t local_x = _falling_piece_x - _board_offset;
+
+	for (uint8_t i = 0; i < _falling_piece.width(); i++) {
+		for (uint8_t j = 0; j < _falling_piece.height(); j++) {
+			if (_falling_piece.get_bit(i, j) && _board.get_bit(local_x + i + offset_x, _falling_piece_y + j + offset_y)) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void TetrisWindow::hold()
+{
+	if (_hold_piece.width() == 0) {
+		_hold_piece = _falling_piece;
+		spawn_piece();
+		return;
+	}
+
+	Bitset2D temp = _falling_piece;
+	_falling_piece = _hold_piece;
+	_hold_piece = temp;
 }
 
 void TetrisWindow::move_piece_left()
 {
+	if (_falling_piece_x > _board_offset && !touching_piece(-1, 0)) _falling_piece_x -= 2;
 }
 
 void TetrisWindow::move_piece_right()
 {
+	if (_falling_piece_x + _falling_piece.width() < _board_offset + _board_width && !touching_piece(1, 0)) _falling_piece_x += 2;
 }
 
 void TetrisWindow::rotate_piece()
 {
+	uint8_t m = _falling_piece.height();
+	uint8_t n = _falling_piece.width();
+
+	Bitset2D new_piece = Bitset2D(m, n, false);
+
+	for (uint8_t i = 0; i < n; i++) {
+		for (uint8_t j = 0; j < m; j++) {
+			new_piece.set_bit(m - j - 1, i, _falling_piece.get_bit(i, j));
+		}
+	}
+
+
+	int8_t x_offset = (_falling_piece.width() - new_piece.width()) / 2;
+	if (x_offset % 2 == 1)
+		x_offset++;
+
+	if (x_offset + _falling_piece_x < _board_offset)
+		_falling_piece_x = _board_offset;
+	else if (_falling_piece_x + new_piece.width() + x_offset > _board_offset + _board_width)
+		_falling_piece_x = _board_offset + _board_width - new_piece.width();
+	else _falling_piece_x += x_offset;
+
+	if (_falling_piece_x % 2 == 1)
+		_falling_piece_x--;
+
+	_falling_piece = new_piece;
 }
--- src/windows/TetrisWindow.h
+++ src/windows/TetrisWindow.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "datastructs/Bitset2D.h"
 #include "utils/Utils.h"
 #include "windows/Window.h"
@@ -12,20 +13,55 @@ class TetrisWindow: public Window
 	bool handle_key_down(KeyPress keypress) override;
 
 	private:
-	Bitset2D board;
-	Bitset2D peices[7] = {
-		Bitset2D(2, 2, { { 0b11000000 }, { 0b11000000 } }),
-		Bitset2D(1, 4, { { 0b11110000 } }),
-		Bitset2D(3, 2, { { 0b10000000 }, { 0b11000000 }, { 0b01000000 } }),
-		Bitset2D(3, 2, { { 0b01000000 }, { 0b11000000 }, { 0b10000000 } }),
-		Bitset2D(2, 3, { { 0b11100000 }, { 0b10000000 } }),
-		Bitset2D(2, 3, { { 0b10000000 }, { 0b11100000 } }),
-		Bitset2D(3, 3, { { 0b01000000 }, { 0b11000000 }, { 0b01000000 } })
+	Bitset2D _pieces[7] = {
+		// # #
+		// # #
+		Bitset2D(4, 4, { { 0b11110000 }, { 0b11110000 }, { 0b11110000 }, { 0b11110000 } }),
+		// #
+		// #
+		// #
+		// #
+		Bitset2D(2, 8, { { 0b11111111 }, { 0b11111111 } }),
+		//   # #
+		// # #
+		Bitset2D(6, 4, { { 0b11000000 }, { 0b11000000 }, { 0b11110000 }, { 0b11110000 }, { 0b00110000 }, { 0b00110000 } }),
+		// # #
+		//   # #
+		Bitset2D(6, 4, { { 0b00110000 }, { 0b00110000 }, { 0b11110000 }, { 0b11110000 }, { 0b11000000 }, { 0b11000000 } }),
+		// #
+		// #
+		// # #
+		Bitset2D(4, 6, { { 0b11111100 }, { 0b11111100 }, { 0b11000000 }, { 0b11000000 } }),
+		//   #
+		//   #
+		// # #
+		Bitset2D(4, 6, { { 0b11000000 }, { 0b11000000 }, { 0b11111100 }, { 0b11111100 } }),
+		//   #
+		// # # #
+		Bitset2D(6, 4, { { 0b11000000 }, { 0b11000000 }, { 0b11110000 }, { 0b11110000 }, { 0b11000000 }, { 0b11000000 } })
 	};
 	void spawn_piece();
 	void move_piece_down();
 	void move_piece_left();
 	void move_piece_right();
 	void rotate_piece();
+	bool touching_piece(int8_t offset_x = 0, int8_t offset_y = 0);
+	void hold();
+	void check_for_lines();
+
+	Bitset2D _board;
 	uint64_t _last_update;
+
+	const uint8_t _board_offset = 38;
+	const uint8_t _board_width = 20;
+
+	uint16_t _score = 0;
+	uint16_t _lines = 0;
+
+	uint8_t _falling_piece_x;
+	uint8_t _falling_piece_y;
+
+	Bitset2D _falling_piece;
+	Bitset2D _next_piece;
+	Bitset2D _hold_piece;
 };
