daf65e1d317c9655fbb5ee4078356e98f55a629a
Emanuel Krollmann <E.Krollmann@protonmail.com>
2024-01-31 21:11:34 +0100

    working on http serialization and deserialization

--- src/http/PicoHttpClient.cpp
+++ src/http/PicoHttpClient.cpp
@@ -1,46 +1,62 @@
 #pragma once
 #include "http/PicoHttpClient.h"
-//#include "lwip/dns.h"
-#include "mbedtls/ssl.h"
 #include "lwip/init.h"
-//#include "lwip/altcp.h"
-//#include "lwip/altcp_tcp.h"
-//#include "lwip/altcp_tls.h"
-//#include "lwip/pbuf.h"
+#include "mbedtls/ssl.h"
 #include <sstream>
 
-#define PICO_WIFI_SSID "G-W0815"
-#define PICO_WIFI_PASSWD "5681 7433 8133 7564"
-#define PICO_WIFI_AUTH CYW43_AUTH_WPA2_AES_PSK
 #define PICO_WIFI_TIMEOUT 200
+#define PICO_WIFI_SSID "TODO"
+#define PICO_WIFI_PASSWORD "PASSWORD"
+#define PICO_WIFI_AUTH 1337
 
 #define HTTP_METHOD_GET 0
 #define HTTP_METHOD_POST 1
 
-PicoHttpClient::PicoHttpClient(std::string baseUrl): IHttpClient(baseUrl){
-    cyw43_arch_init();
-    cyw43_arch_lwip_begin();
-    cyw43_arch_wifi_connect_timeout_ms(PICO_WIFI_SSID, PICO_WIFI_PASSWD, PICO_WIFI_AUTH, PICO_WIFI_TIMEOUT);
-    cyw43_arch_lwip_end();
-    cyw43_arch_lwip_begin();
-    lwip_init();
-    cyw43_arch_lwip_end();
-    baseUrl = baseUrl;
-    cyw43_arch_lwip_begin();
-    this->tls_client = altcp_tls_new(tls_config, IPADDR_TYPE_ANY);
-    cyw43_arch_lwip_end();
-    cyw43_arch_lwip_begin();
-    altcp_arg(this->tls_client, this);
-    cyw43_arch_lwip_end();
-    cyw43_arch_lwip_begin();
-    dns_gethostbyname(baseUrl.c_str(), this->ip_addr, (dns_found_callback)&dns_callback, this);
-    cyw43_arch_lwip_end();
-    cyw43_arch_lwip_begin();
-    altcp_err(this->tls_client, (altcp_err_fn)&err_callback);
-    cyw43_arch_lwip_end();
-    cyw43_arch_lwip_begin();
-    altcp_recv(this->tls_client, (altcp_recv_fn)&recv_callback);
-    cyw43_arch_lwip_end();
+// I know what ur thinking and yes, all of the
+// lwip calls have to be wrapped in cyw43_arch_lwip_begin()
+// and "_end() because lwip API is not thread safe.
+PicoHttpClient::PicoHttpClient(std::string baseUrl):
+	IHttpClient(baseUrl)
+{
+	cyw43_arch_init();
+
+
+	cyw43_arch_lwip_begin();
+	cyw43_arch_wifi_connect_timeout_ms(PICO_WIFI_SSID, PICO_WIFI_PASSWORD, PICO_WIFI_AUTH, PICO_WIFI_TIMEOUT);
+	cyw43_arch_lwip_end();
+
+
+	cyw43_arch_lwip_begin();
+	lwip_init();
+	cyw43_arch_lwip_end();
+
+	this->base_url = baseUrl;
+	this->cert = get_cert(baseUrl);
+
+
+	cyw43_arch_lwip_begin();
+	tls_config = altcp_tls_create_config_client(cert, sizeof(*cert));
+	cyw43_arch_lwip_end();
+
+	cyw43_arch_lwip_begin();
+	this->tls_client = altcp_tls_new(tls_config, IPADDR_TYPE_ANY);
+	cyw43_arch_lwip_end();
+
+	cyw43_arch_lwip_begin();
+	altcp_arg(this->tls_client, this);
+	cyw43_arch_lwip_end();
+
+	cyw43_arch_lwip_begin();
+	dns_gethostbyname(baseUrl.c_str(), this->ip_addr, (dns_found_callback)&dns_callback, this);
+	cyw43_arch_lwip_end();
+
+	cyw43_arch_lwip_begin();
+	altcp_err(this->tls_client, (altcp_err_fn)&err_callback);
+	cyw43_arch_lwip_end();
+
+	cyw43_arch_lwip_begin();
+	altcp_recv(this->tls_client, (altcp_recv_fn)&recv_callback);
+	cyw43_arch_lwip_end();
 }
 
 HttpResponse PicoHttpClient::send_request(HttpRequest req, std::string uri, HttpMethod method)
@@ -52,70 +68,90 @@ HttpResponse PicoHttpClient::send_request(HttpRequest req, std::string uri, Http
 
 	size_t data_pos = 0;
 	std::string data = serialize(req, uri, method);
-    cyw43_arch_lwip_begin();
-	if (data.length() > altcp_sndbuf(this->tls_client)) {
-        cyw43_arch_lwip_end();
-		for (int i = 0 ; data_pos < data.length(); i++) {
-            cyw43_arch_lwip_begin();
-			data_pos += altcp_sndbuf(this->tls_client);
-            cyw43_arch_lwip_end();
-            cyw43_arch_lwip_begin();
-            altcp_write(tls_client, data.substr(data_pos, altcp_sndbuf(this->tls_client)).c_str(), altcp_sndbuf(this->tls_client), 0x00);
-            cyw43_arch_lwip_end();
-            cyw43_arch_lwip_begin();
-			altcp_output(tls_client);
-            cyw43_arch_lwip_end();
-		}
-	}
-    cyw43_arch_lwip_end();
 
+	cyw43_arch_lwip_begin();
+	if (data.length() > altcp_sndbuf(this->tls_client)) {
+		cyw43_arch_lwip_end();
+		for (int i = 0; data_pos < data.length(); i++) {
+			cyw43_arch_lwip_begin();
+			data_pos += altcp_sndbuf(this->tls_client);
+
+			altcp_write(tls_client, data.substr(data_pos, altcp_sndbuf(this->tls_client)).c_str(), altcp_sndbuf(this->tls_client), 0x00);
+
+			altcp_output(tls_client);
+			cyw43_arch_lwip_end();
+		}
+	} else {
+		cyw43_arch_lwip_end();
+		cyw43_arch_lwip_begin();
+        altcp_write(tls_client, data.c_str(), data.length(), 0x00);
+
+        altcp_output(tls_client);
+		cyw43_arch_lwip_end();
+	}
+
+    while(!this->received)
+        sleep_ms(200);
+    
+    HttpResponse response = deserialize(this->response_raw);
+    return response;
 }
 
 std::string PicoHttpClient::serialize(HttpRequest& req, std::string uri, HttpMethod& method)
 {
-    std::string data;
 	std::string method_str;
+    std::ostringstream result;
 	if (method == HttpMethod::Get) {
 		method_str = "GET";
 	} else {
 		method_str = "POST";
 	}
 
-	data += method_str + " " + uri + " HTTP/1.1";
-	for (auto& h : req.headers) {
-		data += h.first + ": " + h.second + "\n";
-	}
-	data += "\n" + req.body;
-	return data;
+    result << method_str << uri << "HTTP/2" << std::endl;
+    for(auto h : req.headers){
+        result << h.first << ": " << h.second << std::endl;
+    }
+
+    result << std::endl;
+    result << req.body << std::endl;
+
+    return result.str();
 }
+
+/*
+ * TODO - this method will get the root certificate for the provided url
+u8_t* PicoHttpClient::get_cert(std::string baseUrl)
+{
+}
+*/
 
 /*
 HttpResponse PicoHttpClient::deserialize(std::string data)
 {
-	HttpResponse res;
-	u16_t status_code;
-	istringstream data_s(data);
+    HttpResponse res;
+    u16_t status_code;
+    istringstream data_s(data);
     std::string l;
-	std::string key, value;
+    std::string key, value;
     std::streamsize lim = std::numeric_limits<std::streamsize>::max();
 
-	data_s >> status_code;
+    data_s >> status_code;
     data_s.ignore(lim, '\n');
 
-	while (std::getline(data_s, l)) {
-		if (l.empty()) {
-			break;
-		}
+    while (std::getline(data_s, l)) {
+        if (l.empty()) {
+            break;
+        }
 
-		istringstream tmp(l);
+        istringstream tmp(l);
 
-		tmp >> key; 
+        tmp >> key;
         tmp.ignore(lim, " ");
         tmp >> value;
         data_s.ignore(lim, '\n');
-	}
+    }
 
-	data_s.read(&res.body, std::find(res.headers.begin(), res.headers.end(), "Content-Length").next()))
+    data_s.read(&res.body, std::find(res.headers.begin(), res.headers.end(), "Content-Length").next()))
 
     return res;
 }
@@ -127,19 +163,17 @@ err_t PicoHttpClient::recieve(struct tcp_pcb* tpcb, struct pbuf* p, err_t err)
 	if (err == ERR_OK) {
 		std::string data;
 
-		// Print packet buffer
 		while (buf->len != buf->tot_len) {
 			data.append(std::string((char*)buf->payload));
 			buf = buf->next;
 		}
 		data.append(std::string((char*)buf->payload));
 
-		// Advertise data reception
-        cyw43_arch_lwip_begin();
-	    altcp_recved(tls_client, buf->tot_len);
-        cyw43_arch_lwip_end();
+		cyw43_arch_lwip_begin();
+		altcp_recved(tls_client, buf->tot_len);
+		cyw43_arch_lwip_end();
 		this->received += buf->tot_len;
-		received = true;
+        this->response_raw = data;
 	}
 
 	pbuf_free(buf);
@@ -161,17 +195,17 @@ err_t PicoHttpClient::connected_fn(struct tcp_pcb* tpcb, err_t err)
 
 err_t PicoHttpClient::dn_found(const char* name, const ip_addr_t* addr)
 {
-    cyw43_arch_lwip_begin();
+	cyw43_arch_lwip_begin();
 	return altcp_connect(tls_client, addr, port, (altcp_connected_fn)&connected_callback);
-    cyw43_arch_lwip_end();
+	cyw43_arch_lwip_end();
 }
 
 
 PicoHttpClient::~PicoHttpClient()
 {
-    cyw43_arch_lwip_begin();
+	cyw43_arch_lwip_begin();
 	altcp_shutdown(tls_client, 1, 1);
-    cyw43_arch_lwip_end();
+	cyw43_arch_lwip_end();
 
-    cyw43_arch_deinit();
+	cyw43_arch_deinit();
 }
--- src/http/PicoHttpClient.h
+++ src/http/PicoHttpClient.h
@@ -2,105 +2,115 @@
 #include "http/HttpRequest.h"
 #include "http/HttpResponse.h"
 #include "http/IHttpClient.h"
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-#include "lwip/pbuf.h"
 #include "lwip/altcp.h"
 #include "lwip/altcp_tcp.h"
 #include "lwip/altcp_tls.h"
 #include "lwip/dns.h"
+#include "lwip/pbuf.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
 #include <string>
-#define CERT 
+#define CERT
 
 enum HttpMethod {
-    Get,
-    Post,
+	Get,
+	Post,
 };
 
 class PicoHttpClient: public IHttpClient
 {
 	public:
-    PicoHttpClient(std::string baseUrl);
-    HttpResponse get(HttpRequest req, std::string path){
-        return send_request(req, path, HttpMethod::Get);
-    };
-    HttpResponse post(HttpRequest req, std::string path){
-        return send_request(req, path, HttpMethod::Post);
-    }
-    void set_bearer_auth_token(std::string token){
-        this->bearer_auth_token = token;
+	PicoHttpClient(std::string baseUrl);
+	HttpResponse get(HttpRequest req, std::string path)
+	{
+		return send_request(req, path, HttpMethod::Get);
+	};
+	HttpResponse post(HttpRequest req, std::string path)
+	{
+		return send_request(req, path, HttpMethod::Post);
+	}
+	void set_bearer_auth_token(std::string token)
+	{
+		this->bearer_auth_token = token;
+	};
 
-    };
+	static err_t recv_callback(void* arg, struct tcp_pcb* tpcb, struct pbuf* p, err_t err)
+	{
+		return static_cast<PicoHttpClient*>(arg)->recieve(tpcb, p, err);
+	}
 
-    static err_t recv_callback(void* arg, struct tcp_pcb* tpcb, struct pbuf* p, err_t err){
-        return static_cast<PicoHttpClient*>(arg)->recieve(tpcb, p, err);
-    }
+	static err_t err_callback(void* arg, err_t err)
+	{
+		return static_cast<PicoHttpClient*>(arg)->client_error(err);
+	}
 
-    static err_t err_callback(void* arg, err_t err){
-        return static_cast<PicoHttpClient*>(arg)->client_error(err);
-    }
-
-    static err_t connected_callback(void* arg, struct tcp_pcb* tpcb, err_t err){
-        return static_cast<PicoHttpClient*>(arg)->connected_fn(tpcb, err);
-    }
+	static err_t connected_callback(void* arg, struct tcp_pcb* tpcb, err_t err)
+	{
+		return static_cast<PicoHttpClient*>(arg)->connected_fn(tpcb, err);
+	}
 
 
-    static err_t dns_callback(const char* name, const ip_addr_t* addr, void* callback_arg){
-        return static_cast<PicoHttpClient*>(callback_arg)->dn_found(name, addr);
-    }
+	static err_t dns_callback(const char* name, const ip_addr_t* addr, void* callback_arg)
+	{
+		return static_cast<PicoHttpClient*>(callback_arg)->dn_found(name, addr);
+	}
 
-    ~PicoHttpClient();
+
+	~PicoHttpClient();
 
 	private:
-    ///<summary>
-    ///sends a request get() and set() are just wrappers around this
-    ///<param name='req'> the HttpRequest to send </param>
-    ///<param name='uri'> the URI to send to</param>
-    ///<param name='method'> the HttpMethod to use</param>
-    ///</summary>
-    HttpResponse send_request(HttpRequest req, std::string uri, HttpMethod method);
-    ///<summary>
-    ///serializes a raw data string into an HttpResponse
-    ///<param name='data'> the data string to put into an HttpRequest
-    ///</summary>
-    HttpResponse deserialize(std::string data);
-    ///<summary>
-    ///serializes an HttpRequest into a raw data string
-    ///<param name='req'> the request to convert </param>
-    ///<param name='uri'> the URI of the request </param>
-    ///<param name='method'> the method of the request </param>
-    ///</summary>
-    std::string serialize(HttpRequest& req, std::string uri, HttpMethod& method);
-    ///<summary>
-    ///altcp receive callback, look at the altcp doc for more info
-    ///</summary>
-    
-    err_t recieve(struct tcp_pcb* tpcb, struct pbuf* p, err_t err);
-    ///<summary>
-    ///altcp connected callback
-    ///</summary>
-    err_t connected_fn(struct tcp_pcb* tpcb, err_t err);
-    ///<summary>
-    ///altcp dns callback
-    ///</summary>
-    
-    err_t dn_found(const char* name, const ip_addr_t* addr);
-    ///<sumamry>
-    ///altcp client error callback
-    ///</sumamry>
-    err_t client_error(err_t err);
-    
-    
-    
-    int content_l = 0;
-    std::string bearer_auth_token;
-    std::string base_url;
+	///< summary>
+	/// sends a request get() and set() are just wrappers around this
+	///< param name='req'> the HttpRequest to send </param>
+	///< param name='uri'> the URI to send to</param>
+	///< param name='method'> the HttpMethod to use</param>
+	///</summary>
+	HttpResponse send_request(HttpRequest req, std::string uri, HttpMethod method);
+	///< summary>
+	/// serializes a raw data string into an HttpResponse
+	///< param name='data'> the data string to put into an HttpRequest
+	///</summary>
+	HttpResponse deserialize(std::string data);
+	///< summary>
+	/// serializes an HttpRequest into a raw data string
+	///< param name='req'> the request to convert </param>
+	///< param name='uri'> the URI of the request </param>
+	///< param name='method'> the method of the request </param>
+	///</summary>
+	std::string serialize(HttpRequest& req, std::string uri, HttpMethod& method);
 
-    int received = 0;
-    bool connected = false;
-    ip_addr_t* ip_addr;
-    u16_t port = 443;
-    struct altcp_tls_config* tls_config = NULL;
-    struct altcp_pcb* tls_client = NULL;
+	u8_t* get_cert(std::string baseUrl);
+
+	///< summary>
+	/// altcp receive callback, look at the altcp doc for more info
+	///</summary>
+	err_t recieve(struct tcp_pcb* tpcb, struct pbuf* p, err_t err);
+
+	///< summary>
+	/// altcp connection callback
+	///</summary>
+	err_t connected_fn(struct tcp_pcb* tpcb, err_t err);
+
+	///< summary>
+	/// altcp dns callback
+	///</summary>
+	err_t dn_found(const char* name, const ip_addr_t* addr);
+
+	///< sumamry>
+	/// altcp client error callback
+	///</sumamry>
+	err_t client_error(err_t err);
+
+
+    std::string response_raw;
+	int content_l = 0;
+	std::string bearer_auth_token;
+	std::string base_url;
+	u8_t* cert;
+	int received = 0;
+	bool connected = false;
+	ip_addr_t* ip_addr;
+	u16_t port = 443;
+	struct altcp_tls_config* tls_config = NULL;
+	struct altcp_pcb* tls_client = NULL;
 };
-
