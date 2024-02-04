4cc9bffd4f2681b347b5fda27a14a58732c23881
Karl Gerhards <67832062+DraqzzIQ@users.noreply.github.com>
2024-02-04 13:15:36 +0100

    add saved app data to gitignore; use root persistence folder for saved data; add preview to README

--- src/io/IOController.cpp
+++ src/io/IOController.cpp
@@ -164,12 +164,16 @@ bool IOController::unmount()
 
 #else
 
+const std::string IOController::_root_dir = "persistence";
+
 bool IOController::write_file(std::string dir, std::string filename, const std::vector<uint8_t>* bytes)
 {
+	dir = _root_dir + "/" + dir;
+
 	if (!std::filesystem::exists(dir))
-		std::filesystem::create_directory(dir);
+		std::filesystem::create_directories(dir);
 	FILE* file = fopen((dir + "/" + filename).c_str(), "wb");
-	if(!file) return false;
+	if (!file) return false;
 	fwrite(bytes->data(), sizeof(uint8_t), bytes->size(), file);
 	fclose(file);
 	return true;
@@ -177,8 +181,10 @@ bool IOController::write_file(std::string dir, std::string filename, const std::
 
 bool IOController::read_file(std::string dir, std::string filename, std::vector<uint8_t>* bytes)
 {
+	dir = _root_dir + "/" + dir;
+
 	FILE* file = fopen((dir + "/" + filename).c_str(), "rb");
-	if(!file) return false;
+	if (!file) return false;
 	fseek(file, 0, SEEK_END);
 	size_t size = ftell(file);
 	bytes->resize(size);
@@ -190,18 +196,19 @@ bool IOController::read_file(std::string dir, std::string filename, std::vector<
 
 bool IOController::file_exists(std::string dir, std::string filename)
 {
-	return std::filesystem::exists(dir + "/" + filename);
+	return std::filesystem::exists(_root_dir + "/" + dir + "/" + filename);
 }
 
 bool IOController::dir_exists(std::string dir)
 {
-	return std::filesystem::exists(dir);
+	return std::filesystem::exists(_root_dir + "/" + dir);
 }
 
 std::vector<std::string> IOController::list_dir(std::string dir)
 {
+	dir = _root_dir + "/" + dir;
 	std::vector<std::string> files;
-	if(!std::filesystem::exists(dir)) return files;
+	if (!std::filesystem::exists(dir)) return files;
 
 	for (const auto& entry : std::filesystem::directory_iterator(dir))
 		files.push_back(entry.path().filename().string());
